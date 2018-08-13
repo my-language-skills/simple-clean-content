3df5c72b7f7922d6e4b70140e4e39249bd9d610a
Christodoulos Demetriou <37778768+tooulakis13@users.noreply.github.com>
2018-08-13 09:56:26 +0200

    Update clean-text-style-tinymce.php

--- clean-text-style-tinymce.php
+++ clean-text-style-tinymce.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    Plugin Name: Clean Text Style
+    Plugin Name: Simple Clean Content
     Description: This plugin, adds a button on TinyMCE toolbal editor. By clicking the button, all the styles of the text and images that the editor contains are removed, leaving just the paragraphs intact. Additionally, extra spaces as well as extra lines are removed.
     Plugin URI: #
     Version: 1.0
