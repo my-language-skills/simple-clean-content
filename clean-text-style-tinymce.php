898bf791995727ba2c51163ad0ca64b2f65d0f94
Christodoulos Demetriou <37778768+tooulakis13@users.noreply.github.com>
2018-08-13 09:56:45 +0200

    Update clean-text-style-tinymce.php

--- clean-text-style-tinymce.php
+++ clean-text-style-tinymce.php
@@ -4,7 +4,7 @@
     Description: This plugin, adds a button on TinyMCE toolbal editor. By clicking the button, all the styles of the text and images that the editor contains are removed, leaving just the paragraphs intact. Additionally, extra spaces as well as extra lines are removed.
     Plugin URI: #
     Version: 1.0
-    License: GPL-2.0
+    License: GPL-3.0
 */
 
 // Add required css file for custom icon
