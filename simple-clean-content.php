c8cca0e5f3f076a9d07a3131096134659d90a6a1
Christodoulos Demetriou <37778768+tooulakis13@users.noreply.github.com>
2018-08-23 10:34:29 +0200

    Update simple-clean-content.php

--- simple-clean-content.php
+++ simple-clean-content.php
@@ -9,14 +9,14 @@
 
 // Add required css file for custom icon
 
-function clean_text_style_tinymce_additional_css() {
+function scc_tinymce_additional_css() {
   wp_enqueue_style( 'clean-text-style-tinymce', plugins_url( 'css/simple-clean-content.css', __FILE__ ) );
 }
-add_action( 'admin_enqueue_scripts', 'clean_text_style_tinymce_additional_css' );
+add_action( 'admin_enqueue_scripts', 'scc_tinymce_additional_css' );
 
 // Hook the plugin file for TinyMCE and the button to the editor
 
-function clean_text_style_tinymce_main() {
+function scc_tinymce_main() {
   global $typenow;
   // check the current user's permissions
   if ( !current_user_can('edit_posts') && !current_user_can('edit_pages') ) {
@@ -30,22 +30,22 @@ function clean_text_style_tinymce_main() {
 
   // check if WYSIWYG is enabled
   if ( get_user_option('rich_editing') == 'true' ) {
-    add_filter( 'mce_external_plugins', 'clean_text_style_tinymce_add_plugin' );
-    add_filter( 'mce_buttons_2', 'clean_text_style_tinymce_register_buttons' );
+    add_filter( 'mce_external_plugins', 'scc_tinymce_add_plugin' );
+    add_filter( 'mce_buttons_2', 'scc_tinymce_register_buttons' );
   }
 }
-add_action('admin_head', 'clean_text_style_tinymce_main');
+add_action('admin_head', 'scc_tinymce_main');
 
 // Add TinyMCE plugin file to TinyMCE editor
 
-function clean_text_style_tinymce_add_plugin( $plugin_array ) {
+function scc_tinymce_add_plugin( $plugin_array ) {
   $plugin_array['clean-text-style-tinymce-plugin'] = plugins_url( 'js/simple-clean-content.js', __FILE__ );
   return $plugin_array;
 }
 
 // Adds the button defined in the plugin file to the button list on TinyMCE
 
-function clean_text_style_tinymce_register_buttons( $buttons ) {
+function scc_tinymce_register_buttons( $buttons ) {
   array_push( $buttons, 'clean_text_style_tinymce_custom_icon_button' );
   return $buttons;
 }
