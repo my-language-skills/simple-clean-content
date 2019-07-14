3239bf042f7eb88d8842ae50cadfd015acb0b74b
Colomet <colomet@hotmail.com>
2019-07-14 19:07:21 +0200

    PHP Documentation

--- simple-clean-content.php
+++ simple-clean-content.php
@@ -25,14 +25,26 @@
  * Domain Path:       /languages
  */
 
-// Add required css file for custom icon
+ /**
+  * Add required css file for custom icon
+  *
+  * @since
+  *
+  */
+
 function scc_tinymce_additional_css() {
   wp_enqueue_style( 'clean-text-style-tinymce', plugins_url( 'css/simple-clean-content.css', __FILE__ ) );
 }
 //Load css for all admin pages. It is the first action hooked into the admin scripts actions
 add_action( 'admin_enqueue_scripts', 'scc_tinymce_additional_css' );
 
-// Hook the plugin file for TinyMCE and the button to the editor
+/**
+ * Hook the plugin file for TinyMCE and the button to the editor
+ *
+ * @since
+ *
+ */
+
 function scc_tinymce_main() {
   global $typenow;
   // check the current user's permissions
@@ -53,13 +65,25 @@ function scc_tinymce_main() {
 }
 add_action('admin_head', 'scc_tinymce_main');
 
-// Add TinyMCE plugin file to TinyMCE editor
+/**
+ * Add TinyMCE plugin file to TinyMCE editor
+ *
+ * @since
+ *
+ */
+
 function scc_tinymce_add_plugin( $plugin_array ) {
   $plugin_array['scc-tinymce-plugin'] = plugins_url( 'js/simple-clean-content.js', __FILE__ );
   return $plugin_array;
 }
 
-// Adds the button defined in the plugin file to the button list on TinyMCE
+/**
+ * Adds the button defined in the plugin file to the button list on TinyMCE
+ *
+ * @since
+ *
+ */
+ 
 function scc_tinymce_register_buttons( $buttons ) {
   array_push( $buttons, 'scc_tinymce_custom_icon_button' );
   return $buttons;
