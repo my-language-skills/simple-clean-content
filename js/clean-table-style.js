7485ad5ca7e3610acf8c27c9b81acbf54ae48baa
Arafatmollik1 <arafathossain923@gmail.com>
2020-09-09 17:35:07 +0300

    Update clean-table-style.js

--- js/clean-table-style.js
+++ js/clean-table-style.js
@@ -1,12 +1,7 @@
 QTags.addButton('cleanTableStyle', 'cleanTableStyle', do_something);
 function do_something() {
   ( function( ) {
-    console.log("Works1");
- 
-      console.log("Works2");
       ( function(editor) {
-        console.log("Works3");
-        //var node = editor.selection.getStart();
         var table = editor.dom.getParents(  'table' );
         var attr = {
           style: null,
@@ -27,13 +22,10 @@ function do_something() {
           cellspacing: null,
           cellpadding: null
         };
-        console.log("Works4");
         if ( table ) {
-          console.log("Works5");
           editor.$( table ).attr( attr ).find( 'tr, th, td, thead, tbody, tfoot' ).each( function( i, element ) {
             editor.$( element ).attr( attr );
           } );
-          console.log("Works6");
         }
       }( window.tinymce ));
 
